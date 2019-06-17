2a8cb7e200198edc42e6c8b6371a1388a8298a4b
liubaoshuai <liubaoshuai1@jd.com>
2019-06-17 16:28:49 +0800

    update

--- MyBasicSkillApp/SecondaryPage/Constraint/Controllers/MBSConstraintViewController.m
+++ MyBasicSkillApp/SecondaryPage/Constraint/Controllers/MBSConstraintViewController.m
@@ -21,29 +21,8 @@
     
     _mbConstraintView = [[MBSConstraintView alloc] initWithFrame:CGRectZero];
     _mbConstraintView.backgroundColor = [UIColor grayColor];
-//    [self.view addSubview:_mbConstraintView];
-    
-    
-    UIView *subView5 = [[UIView alloc] initWithFrame:CGRectZero];
-    subView5.backgroundColor = [[UIColor cyanColor] colorWithAlphaComponent:.6];
-    //Step 1 - 使用NSLayoutConstraint布局前，要先指明该视图不被Autoresizing控制，否则会出现约束冲突情况
-    subView5.translatesAutoresizingMaskIntoConstraints = NO;
-    [self.view addSubview:subView5];
-    
-    
-    //Step 2 - 设置约束
-    NSLayoutConstraint *widthConstraint = [NSLayoutConstraint constraintWithItem:subView5 attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0 constant:100.0];
-    
-    NSLayoutConstraint *heightConstraint = [NSLayoutConstraint constraintWithItem:subView5 attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0 constant:200.0];
-    
-    NSLayoutConstraint *centerXConstraint = [NSLayoutConstraint constraintWithItem:subView5 attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1.0 constant:.0];
-    
-    NSLayoutConstraint *centerYConstraint = [NSLayoutConstraint constraintWithItem:subView5 attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterY multiplier:1.0 constant:.0];
-    
-       
-    
-     //Step 3 - 添加约束
-    [self.view addConstraints:@[widthConstraint, heightConstraint, centerXConstraint, centerYConstraint]];
+    [self.view addSubview:_mbConstraintView];
+
 }
 
 
--- MyBasicSkillApp/SecondaryPage/Constraint/Views/MBSConstraintView.m
+++ MyBasicSkillApp/SecondaryPage/Constraint/Views/MBSConstraintView.m
@@ -28,56 +28,61 @@
     self = [super initWithFrame:frame];
     
     if (self) {
-        
-        _subView1 = [[UIView alloc] initWithFrame:CGRectZero];
-        _subView1.backgroundColor = [[UIColor redColor] colorWithAlphaComponent:.6];
-        [self addSubview:_subView1];
-        
-        _subView2 = [[UIView alloc] initWithFrame:CGRectZero];
-        _subView2.backgroundColor = [[UIColor greenColor] colorWithAlphaComponent:.6];
-        [self addSubview:_subView2];
-        
-        _subView3 = [[UIView alloc] initWithFrame:CGRectZero];
-        _subView3.backgroundColor = [[UIColor blueColor] colorWithAlphaComponent:.6];
-        [self addSubview:_subView3];
-        
-        _subView4 = [[UIView alloc] initWithFrame:CGRectZero];
-        _subView4.backgroundColor = [[UIColor yellowColor] colorWithAlphaComponent:.6];
-        [self addSubview:_subView4];
-        
-        _subView5 = [[UIView alloc] initWithFrame:CGRectZero];
-        _subView5.backgroundColor = [[UIColor cyanColor] colorWithAlphaComponent:.6];
-        _subView5.translatesAutoresizingMaskIntoConstraints = NO;
-        [self addSubview:_subView5];
+        [self setUpUI];
+        [self setUpConstraint];
     }
     
     return self;
 }
 
 
-- (void)layoutSubviews {
+
+
+- (void) setUpUI {
+    _subView1 = [[UIView alloc] initWithFrame:CGRectZero];
+    _subView1.backgroundColor = [[UIColor redColor] colorWithAlphaComponent:.6];
+    [self addSubview:_subView1];
     
-    NSLog(@"-----%s", __FUNCTION__);
+    _subView2 = [[UIView alloc] initWithFrame:CGRectZero];
+    _subView2.backgroundColor = [[UIColor greenColor] colorWithAlphaComponent:.6];
+    [self addSubview:_subView2];
     
-    [super layoutSubviews];
+    _subView3 = [[UIView alloc] initWithFrame:CGRectZero];
+    _subView3.backgroundColor = [[UIColor blueColor] colorWithAlphaComponent:.6];
+    [self addSubview:_subView3];
     
-    CGFloat margin = 20.0;
-    CGFloat padding = 20.0;
-    CGFloat width = (self.bounds.size.width - margin * 2 - padding) / 2;
-    CGFloat height = (self.bounds.size.height - margin * 2 - padding) / 2;
+    _subView4 = [[UIView alloc] initWithFrame:CGRectZero];
+    _subView4.backgroundColor = [[UIColor yellowColor] colorWithAlphaComponent:.6];
+    [self addSubview:_subView4];
     
-    _subView1.frame = CGRectMake(margin, margin, width, height);
-    _subView2.frame = CGRectMake(margin + width + padding, margin, width, height);
-    _subView3.frame = CGRectMake(margin, margin + height + padding, width, height);
-    _subView4.frame = CGRectMake(margin + width + padding, margin + height + padding, width, height);
+    _subView5 = [[UIView alloc] initWithFrame:CGRectZero];
+    _subView5.backgroundColor = [[UIColor cyanColor] colorWithAlphaComponent:.6];
+
+    //Step 1 - 使用NSLayoutConstraint布局前，要先指明该视图不被Autoresizing控制，否则会出现约束冲突情况
+    _subView5.translatesAutoresizingMaskIntoConstraints = NO;
+    [self addSubview:_subView5];
+}
+
+- (void) setUpConstraint {
+   
+
     
     
+    //设置约束
     NSLayoutConstraint *widthConstraint = [NSLayoutConstraint constraintWithItem:_subView5 attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0 constant:100.0];
+    
     NSLayoutConstraint *heightConstraint = [NSLayoutConstraint constraintWithItem:_subView5 attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0 constant:200.0];
+    
     NSLayoutConstraint *centerXConstraint = [NSLayoutConstraint constraintWithItem:_subView5 attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeCenterX multiplier:1.0 constant:.0];
+    
     NSLayoutConstraint *centerYConstraint = [NSLayoutConstraint constraintWithItem:_subView5 attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeCenterY multiplier:1.0 constant:.0];
     
-     [self addConstraints:@[widthConstraint, heightConstraint, centerXConstraint, centerYConstraint]];}
+    
+    
+    //Step 3 - 添加约束
+    [self addConstraints:@[widthConstraint, heightConstraint, centerXConstraint, centerYConstraint]];
+}
+
 
 
 @end
