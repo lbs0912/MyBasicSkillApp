7034a03a50c95a2c8f5f9ef7d73869e6a46b2e8e
liubaoshuai <liubaoshuai1@jd.com>
2019-06-17 21:55:51 +0800

    add VFL autolayout demo

--- MyBasicSkillApp/SecondaryPage/Constraint/Views/MBSConstraintView.m
+++ MyBasicSkillApp/SecondaryPage/Constraint/Views/MBSConstraintView.m
@@ -28,9 +28,29 @@
     
     self = [super initWithFrame:frame];
     
+   
+    /**
+     * techKind - 使用何种技术实现 Constraint 布局
+     * 1-使用系统代码添加约束
+     * 2-使用VFL
+     */
+    NSInteger techKind = 2;
+    
+    
     if (self) {
-        [self setUpUI]; //Step 0，1
-        [self setUpConstraint]; //Step 2，3
+        switch (techKind) {
+            case 1:
+                [self setUpUI];    //1-使用系统代码添加约束
+                [self setUpConstraint]; //Step 2，3
+                break;
+            case 2:
+                [self setUpUI];   //2-VFL
+                [self setUpConstraintWithVFL]; 
+            default:
+                break;
+        }
+        
+       
         //Step 4 - 刷新约束
         [self layoutIfNeeded];
     }
@@ -72,8 +92,101 @@
     _subView5.translatesAutoresizingMaskIntoConstraints = NO;
 }
 
+
+
+// 使用VFL 进行约束
+- (void) setUpConstraintWithVFL {
+    NSNumber *verticalMargin = @(100);  //垂直方向边距
+    NSNumber *spacing = @(20); //内边距
+    
+    //添加水平方向的约束1
+    //subview1 和subview2 水平方向约束  subview2和subview1 等宽
+    NSString *hFormat1 = @"H:|-spacing-[_subView1]-spacing-[_subView2(==_subView1)]-spacing-|";
+    NSDictionary *hMetrics1 = NSDictionaryOfVariableBindings(spacing);
+    NSDictionary *hVFLView1 = NSDictionaryOfVariableBindings(_subView1,_subView2);
+    NSArray *hConstraints1 = [NSLayoutConstraint constraintsWithVisualFormat:hFormat1
+                                                                     options:NSLayoutFormatAlignAllCenterY
+                                                                     metrics:hMetrics1
+                                                                       views:hVFLView1];
+     [self addConstraints:hConstraints1];
+    
+    //添加水平方向的约束1
+    //subview3 和subview4 水平方向约束  subview3和subview4 等宽
+    NSString *hFormat2 = @"H:|-spacing-[_subView3]-spacing-[_subView4(==_subView3)]-spacing-|";
+    NSDictionary *hMetrics2 = NSDictionaryOfVariableBindings(spacing);
+    NSDictionary *hVFLView2 = NSDictionaryOfVariableBindings(_subView3,_subView4);
+    NSArray *hConstraints2 = [NSLayoutConstraint constraintsWithVisualFormat:hFormat2
+                                                                     options:NSLayoutFormatAlignAllCenterY
+                                                                     metrics:hMetrics2
+                                                                       views:hVFLView2];
+    [self addConstraints:hConstraints2];
+
+
+
+    //添加竖直方向的约束1
+    //subview1 和subview3 竖直方向约束  subview1和subview3 等高
+    NSString *vFormat1 = @"V:|-verticalMargin-[_subView1]-spacing-[_subView3(==_subView1)]-verticalMargin-|";
+    NSDictionary *vMetrics1 = NSDictionaryOfVariableBindings(verticalMargin,spacing);
+    NSDictionary *vVFLView1 = NSDictionaryOfVariableBindings(_subView1,_subView3);
+    NSArray *vConstraints1 = [NSLayoutConstraint constraintsWithVisualFormat:vFormat1
+                                                                     options:NSLayoutFormatAlignAllCenterX
+                                                                     metrics:vMetrics1
+                                                                       views:vVFLView1];
+    [self addConstraints:vConstraints1];
+
+    //添加竖直方向的约束2
+    //subview2 和subview4 竖直方向约束  subview2和subview4 等高
+    NSString *vFormat2 = @"V:|-verticalMargin-[_subView2]-spacing-[_subView4(==_subView2)]-verticalMargin-|";
+    NSDictionary *vMetrics2 = NSDictionaryOfVariableBindings(verticalMargin,spacing);
+    NSDictionary *vVFLView2 = NSDictionaryOfVariableBindings(_subView2,_subView4);
+    NSArray *vConstraints2 = [NSLayoutConstraint constraintsWithVisualFormat:vFormat2
+                                                                     options:NSLayoutFormatAlignAllLeft
+                                                                     metrics:vMetrics2
+                                                                       views:vVFLView2];
+    [self addConstraints:vConstraints2];
+    
+    //设置约束 subview 5
+    NSLayoutConstraint *subView5WidthConstraint = [NSLayoutConstraint constraintWithItem:_subView5
+                                                                               attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual
+                                                                                  toItem:nil
+                                                                               attribute:NSLayoutAttributeNotAnAttribute
+                                                                              multiplier:1.0
+                                                                                constant:100.0];
+    
+    NSLayoutConstraint *subView5HeightConstraint = [NSLayoutConstraint constraintWithItem:_subView5
+                                                                                attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual
+                                                                                   toItem:nil
+                                                                                attribute:NSLayoutAttributeNotAnAttribute
+                                                                               multiplier:1.0
+                                                                                 constant:200.0];
+    
+    NSLayoutConstraint *subView5CenterXConstraint = [NSLayoutConstraint constraintWithItem:_subView5
+                                                                                 attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual
+                                                                                    toItem:self
+                                                                                 attribute:NSLayoutAttributeCenterX
+                                                                                multiplier:1.0
+                                                                                  constant:0.0];
+    
+    NSLayoutConstraint *subView5CenterYConstraint = [NSLayoutConstraint constraintWithItem:_subView5
+                                                                                 attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual
+                                                                                    toItem:self
+                                                                                 attribute:NSLayoutAttributeCenterY
+                                                                                multiplier:1.0
+                                                                                  constant:0.0];
+    //Step 3 - 添加约束
+    [self addConstraints:@[subView5WidthConstraint,
+                           subView5HeightConstraint,
+                           subView5CenterXConstraint,
+                           subView5CenterYConstraint]];
+    
+    
+    
+    
+    
+}
+
 - (void) setUpConstraint {
-    //Step 2 - 设置约束
+    //Step 2 - 设置约
     
     //subView1的top 距离superView的top
     NSLayoutConstraint *subView1TopToSuperViewTop = [NSLayoutConstraint constraintWithItem:_subView1
@@ -275,10 +388,6 @@
                             subView5HeightConstraint,
                             subView5CenterXConstraint,
                             subView5CenterYConstraint]];
-                                                  
-
-
-    
     
 }
 
